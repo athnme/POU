6300e8e00334c0090e088fa2bb42501ec386283e
athnme <a.oezbanazi@gmail.com>
2020-12-23 19:57:58 +0100

    Add google maps with api

--- components/screens/PostScreen.js
+++ components/screens/PostScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
 import styled from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
 import {
   HeaderSix,
@@ -88,6 +89,7 @@ const Map = styled.View`
   background-color: black;
   height: 360px;
   border-radius: 16px;
+  overflow: hidden;
 `;
 
 const RouteContainer = styled.View`
@@ -183,7 +185,236 @@ export default function PostScreen({ route, navigation }) {
           </ButtonContainer>
         </LikeContainer>
         <MapContainer>
-          <Map></Map>
+          <Map>
+            <MapView
+              style={styles.map}
+              scrollEnabled={false}
+              zoomEnabled={false}
+              zoomTapEnabled={false}
+              loadingEnabled={true}
+              liteMode={true}
+              provider={PROVIDER_GOOGLE}
+              customMapStyle={[
+                {
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#212121",
+                    },
+                  ],
+                },
+                {
+                  elementType: "labels.icon",
+                  stylers: [
+                    {
+                      visibility: "off",
+                    },
+                  ],
+                },
+                {
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#757575",
+                    },
+                  ],
+                },
+                {
+                  elementType: "labels.text.stroke",
+                  stylers: [
+                    {
+                      color: "#212121",
+                    },
+                  ],
+                },
+                {
+                  featureType: "administrative",
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#757575",
+                    },
+                  ],
+                },
+                {
+                  featureType: "administrative.country",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#9e9e9e",
+                    },
+                  ],
+                },
+                {
+                  featureType: "administrative.land_parcel",
+                  stylers: [
+                    {
+                      visibility: "off",
+                    },
+                  ],
+                },
+                {
+                  featureType: "administrative.locality",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#bdbdbd",
+                    },
+                  ],
+                },
+                {
+                  featureType: "poi",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#757575",
+                    },
+                  ],
+                },
+                {
+                  featureType: "poi.business",
+                  stylers: [
+                    {
+                      visibility: "off",
+                    },
+                  ],
+                },
+                {
+                  featureType: "poi.park",
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#181818",
+                    },
+                  ],
+                },
+                {
+                  featureType: "poi.park",
+                  elementType: "labels.text",
+                  stylers: [
+                    {
+                      visibility: "off",
+                    },
+                  ],
+                },
+                {
+                  featureType: "poi.park",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#616161",
+                    },
+                  ],
+                },
+                {
+                  featureType: "poi.park",
+                  elementType: "labels.text.stroke",
+                  stylers: [
+                    {
+                      color: "#1b1b1b",
+                    },
+                  ],
+                },
+                {
+                  featureType: "road",
+                  elementType: "geometry.fill",
+                  stylers: [
+                    {
+                      color: "#2c2c2c",
+                    },
+                  ],
+                },
+                {
+                  featureType: "road",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#8a8a8a",
+                    },
+                  ],
+                },
+                {
+                  featureType: "road.arterial",
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#373737",
+                    },
+                  ],
+                },
+                {
+                  featureType: "road.highway",
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#3c3c3c",
+                    },
+                  ],
+                },
+                {
+                  featureType: "road.highway.controlled_access",
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#4e4e4e",
+                    },
+                  ],
+                },
+                {
+                  featureType: "road.local",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#616161",
+                    },
+                  ],
+                },
+                {
+                  featureType: "transit",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#757575",
+                    },
+                  ],
+                },
+                {
+                  featureType: "water",
+                  elementType: "geometry",
+                  stylers: [
+                    {
+                      color: "#000000",
+                    },
+                  ],
+                },
+                {
+                  featureType: "water",
+                  elementType: "labels.text.fill",
+                  stylers: [
+                    {
+                      color: "#3d3d3d",
+                    },
+                  ],
+                },
+              ]}
+              showsUserLocation
+              region={{
+                latitude: post.latitude,
+                longitude: post.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              }}
+            />
+            <Marker
+              coordinate={{
+                latitude: post.latitude,
+                longitude: post.longitude,
+              }}
+              title={"hello"}
+              description={"kdkdkdk"}
+              image={require("../../assets/icon.png")}
+            />
+          </Map>
         </MapContainer>
       </ScrollView>
       <RouteContainer>
@@ -205,5 +436,9 @@ export default function PostScreen({ route, navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  map: {
+    flex: 1,
+    borderRadius: 16,
   },
 });
--- components/screens/RouteScreen.js
+++ components/screens/RouteScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 const MapContainer = styled.View`
   flex: 1;
@@ -11,13 +12,231 @@ const MapContainer = styled.View`
 export default function RouteScreen() {
   return (
     <LinearGradient colors={["#07211F", "#030D12"]} style={styles.container}>
-      <MapContainer></MapContainer>
+      <MapContainer>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={[
+            {
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#212121",
+                },
+              ],
+            },
+            {
+              elementType: "labels.icon",
+              stylers: [
+                {
+                  visibility: "off",
+                },
+              ],
+            },
+            {
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#757575",
+                },
+              ],
+            },
+            {
+              elementType: "labels.text.stroke",
+              stylers: [
+                {
+                  color: "#212121",
+                },
+              ],
+            },
+            {
+              featureType: "administrative",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#757575",
+                },
+              ],
+            },
+            {
+              featureType: "administrative.country",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#9e9e9e",
+                },
+              ],
+            },
+            {
+              featureType: "administrative.land_parcel",
+              stylers: [
+                {
+                  visibility: "off",
+                },
+              ],
+            },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#bdbdbd",
+                },
+              ],
+            },
+            {
+              featureType: "poi",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#757575",
+                },
+              ],
+            },
+            {
+              featureType: "poi.business",
+              stylers: [
+                {
+                  visibility: "off",
+                },
+              ],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#181818",
+                },
+              ],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text",
+              stylers: [
+                {
+                  visibility: "off",
+                },
+              ],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#616161",
+                },
+              ],
+            },
+            {
+              featureType: "poi.park",
+              elementType: "labels.text.stroke",
+              stylers: [
+                {
+                  color: "#1b1b1b",
+                },
+              ],
+            },
+            {
+              featureType: "road",
+              elementType: "geometry.fill",
+              stylers: [
+                {
+                  color: "#2c2c2c",
+                },
+              ],
+            },
+            {
+              featureType: "road",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#8a8a8a",
+                },
+              ],
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#373737",
+                },
+              ],
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#3c3c3c",
+                },
+              ],
+            },
+            {
+              featureType: "road.highway.controlled_access",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#4e4e4e",
+                },
+              ],
+            },
+            {
+              featureType: "road.local",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#616161",
+                },
+              ],
+            },
+            {
+              featureType: "transit",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#757575",
+                },
+              ],
+            },
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [
+                {
+                  color: "#000000",
+                },
+              ],
+            },
+            {
+              featureType: "water",
+              elementType: "labels.text.fill",
+              stylers: [
+                {
+                  color: "#3d3d3d",
+                },
+              ],
+            },
+          ]}
+          showsUserLocation
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </MapContainer>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  map: {
     flex: 1,
   },
 });
